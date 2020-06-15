a1f0045aeb5bf4bd80b25db79293da451b7e5a1c
naoyutan <35648848+naoyutan@users.noreply.github.com>
2020-06-15 21:33:15 +0900

    ver2.0

--- shifter-github-hosting-plugin-sample.php
+++ shifter-github-hosting-plugin-sample.php
@@ -4,7 +4,7 @@ Plugin Name: Shifter GitHub hosting plugin sample
 Plugin URI: https://github.com/getshifter/shifter-github-hosting-plugin-sample/
 Description: Shifter GitHub hosting plugin sample
 Author: Shifter Team
-Version: {release version}
+Version: 2.0
 Author URI: https://getshifter.io/
 */
 require __DIR__ . '/vendor/autoload.php';
